8f338582799e928749658db11ab209286c178ef3
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2024-03-23 19:39:42 +0000

    Dashboard Screen (not complete)

--- app/src/main/java/com/roliveira/spendaholic/ui/MainScreen.kt
+++ app/src/main/java/com/roliveira/spendaholic/ui/MainScreen.kt
@@ -1,11 +1,18 @@
 package com.roliveira.spendaholic.ui
 
+import android.app.Activity
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.colorResource
+import androidx.core.view.WindowCompat
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.roliveira.spendaholic.R
 import com.roliveira.spendaholic.ui.screens.BottomBar
 
 @Composable
@@ -13,6 +20,18 @@ fun MainScreen(viewModel: MainViewModel) {
     val controller: NavController = rememberNavController()
     val navBackStackEntry by controller.currentBackStackEntryAsState()
     val currentRoute = navBackStackEntry?.destination?.route
+
+    val view = LocalView.current
+    val window = (view.context as Activity).window
+
+    if (currentRoute == "dashboard") {
+        window.statusBarColor = colorResource(id = R.color.heavy_blue).toArgb()
+        WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
+
+    } else {
+        window.statusBarColor = Color.White.toArgb()
+        WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
+    }
 
     Scaffold(
         bottomBar = { BottomBar(currentRoute = currentRoute, controller = controller) }
