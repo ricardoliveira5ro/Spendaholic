16201f96dc6e8ed6a8d63ff17b7f448a878fa157
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2024-04-25 19:02:50 +0100

    Average expenses

--- app/src/main/java/com/roliveira/spendaholic/ui/screens/stats/Stats.kt
+++ app/src/main/java/com/roliveira/spendaholic/ui/screens/stats/Stats.kt
@@ -1,6 +1,5 @@
 package com.roliveira.spendaholic.ui.screens.stats
 
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
